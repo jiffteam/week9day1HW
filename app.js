38cb42a746cc86923dbf4d102ebd4a2bb556ff4f
kameron <kamscott3@gmail.com>
2022-05-31 00:51:41 -0400

    mmmoo

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://new-user:test1234@cluster0.6kfgc.mongodb.net/node-auth';
+const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
